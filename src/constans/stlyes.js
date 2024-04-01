3c0caaf302a1795b01963dbb555662e672c0b89f
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-04-01 11:27:21 +0200

    add global style

---
+++ src/constans/stlyes.js
@@ -0,0 +1,10 @@
+export const GlobalStyles = {
+    colors: {
+      primary50: '#e4d9fd',     
+      accent500: '#f7bc0c',
+      red50: '#fcc4e4',
+      red500: 'rgb(244 63 94)',
+      gray500: '#39324a',    
+      blue500:'rgb(8 145 178);'
+    },
+  };
