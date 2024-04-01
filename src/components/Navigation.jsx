9fb433309ba0b4d06f9c6e2b11699809d63431a7
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-04-01 11:27:40 +0200

    add navigation

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,78 @@
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import HomeScreen from "../pages/HomeScreen";
+import FavoriteScreen from "../pages/FavoriteScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { Text } from "react-native";
+import { GlobalStyles } from "../constans/stlyes";
+
+const Drawer = createDrawerNavigator();
+
+const CustomHeaderMovieTitle = () => {
+  return (
+    <Text
+      style={{
+        fontSize: 24,
+        fontWeight: "bold",
+        color: GlobalStyles.colors.primary50,
+      }}
+    >
+      <Text style={{ color: GlobalStyles.colors.red500 }}>M</Text>
+      <Text>ovie</Text>
+    </Text>
+  );
+};
+
+const CustomHeaderFavoritesTitle = () => {
+  return (
+    <Text
+      style={{
+        fontSize: 24,
+        fontWeight: "bold",
+        color: GlobalStyles.colors.primary50,
+      }}
+    >
+      <Text style={{ color: GlobalStyles.colors.red500 }}>F</Text>
+      <Text>avorites</Text>
+    </Text>
+  );
+};
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: GlobalStyles.colors.gray500,
+          },
+          headerTitleAlign: "center",
+          headerTintColor: GlobalStyles.colors.primary50,
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          drawerContentStyle: { backgroundColor: GlobalStyles.colors.gray500 },
+          drawerInactiveTintColor: GlobalStyles.colors.primary50,
+          drawerActiveBackgroundColor: GlobalStyles.colors.blue500,
+          drawerActiveTintColor: GlobalStyles.colors.accent500,
+        }}
+      >
+        <Drawer.Screen
+          name="Movie"
+          component={HomeScreen}
+          options={{
+            headerTitle: (props) => <CustomHeaderMovieTitle {...props} />,
+          }}
+        />
+        <Drawer.Screen
+          name="Favorites"
+          component={FavoriteScreen}
+          options={{
+            headerTitle: (props) => <CustomHeaderFavoritesTitle {...props} />,
+          }}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
