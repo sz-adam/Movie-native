302d0f603ba67ca3e8370ee8d8763877f91ec552
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-04-02 10:27:42 +0200

    navigate and style

--- src/components/MovieCarousel.jsx
+++ src/components/MovieCarousel.jsx
@@ -1,15 +1,20 @@
+import { useNavigation } from "@react-navigation/native";
 import React, { useRef } from "react";
 import {
   View,
   Image,
+  Text,
   useWindowDimensions,
   ScrollView,
+  TouchableOpacity,
 } from "react-native";
+import { GlobalStyles } from "../constans/stlyes";
 
 const MovieCarousel = ({ data }) => {
   const { width } = useWindowDimensions();
   const SIZE = width * 0.6;
   const scrollViewRef = useRef();
+  const navigation = useNavigation();
 
   const handleScroll = (event) => {
     const contentOffsetX = event.nativeEvent.contentOffset.x;
@@ -22,31 +27,48 @@ const MovieCarousel = ({ data }) => {
     }
   };
 
+  const handleClick = (item) => {
+    navigation.navigate("MovieDetails", item);
+  };
+
   return (
-    <ScrollView
-      ref={scrollViewRef}
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      bounces={false}
-      scrollEventThrottle={16}
-      snapToInterval={SIZE}
-      decelerationRate="fast"
-      onScroll={handleScroll}
-    >
-    
-      {data.map((item) => (
-        <View key={item.id} style={{ width: SIZE * 0.7 }}>
-          <View className="rounded-3xl overflow-hidden m-2">
-            <Image
-              source={item.imageUrl}
-              className="w-full aspect-square "
-              style={{ height: undefined }}
-            />
+    <View>
+      <Text
+        className="text-center text-2xl font-bold"
+        style={{ color: GlobalStyles.colors.red500 }}
+      >
+        N<Text style={{ color: GlobalStyles.colors.primary50 }}>ewest</Text>
+      </Text>
+      <ScrollView
+        ref={scrollViewRef}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        bounces={false}
+        scrollEventThrottle={16}
+        snapToInterval={SIZE}
+        decelerationRate="fast"
+        onScroll={handleScroll}
+      >
+        {data.map((item) => (
+          <View key={item.id} style={{ width: SIZE * 0.9 }}>
+            <TouchableOpacity onPress={() => handleClick(item)}>
+              <View className="rounded-3xl overflow-hidden m-2">
+                <Image
+                  source={item.imageUrl}
+                  className="w-full aspect-square "
+                  style={{ height: undefined }}
+                />
+                <Text className="absolute right-3 top-1">O</Text>
+                <Text className="absolute left-2 bottom-5 color-white">
+                  Lorem ipsum dolor sit amet.
+                </Text>
+              </View>
+            </TouchableOpacity>
           </View>
-        </View>
-      ))}    
-      <View style={{ width: SIZE * 0.7 }}></View>
-    </ScrollView>
+        ))}
+        <View style={{ width: SIZE * 0.7 }}></View>
+      </ScrollView>
+    </View>
   );
 };
 
