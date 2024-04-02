988e21f165cf3458f29dc48bf7b2965408ecdbd5
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-04-02 10:24:54 +0200

    repair name

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,19 +1,20 @@
 import { View, Text, ScrollView } from "react-native";
 import React, { useState } from "react";
 import MovieCarousel from "../components/MovieCarousel";
+import { GlobalStyles } from "../constans/stlyes";
 
 const HomeScreen = () => {
 
   const data = [
-    { imageUrl: require("../../assets/i1.jpeg") },
-    { imageUrl: require("../../assets/i2.jpeg") },
-    { imageUrl: require("../../assets/i3.jpeg") },
-    { imageUrl: require("../../assets/i2.jpeg") },
-    { imageUrl: require("../../assets/i1.jpeg") },
-    { imageUrl: require("../../assets/i2.jpeg") },
+    {id:1, imageUrl: require("../../assets/i1.jpeg") },
+    {id:2, imageUrl: require("../../assets/i2.jpeg") },
+    {id:3, imageUrl: require("../../assets/i3.jpeg") },
+    {id:4, imageUrl: require("../../assets/i2.jpeg") },
+    {id:5, imageUrl: require("../../assets/i1.jpeg") },
+    {id:6, imageUrl: require("../../assets/i2.jpeg") },
   ];
   return (
-    <View className="flex-1 items-center justify-center">
+    <View className="flex-1 items-center justify-center" style={{backgroundColor:GlobalStyles.colors.gray500}}>
 
    
         <MovieCarousel data={data}/>
