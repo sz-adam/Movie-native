c5648ada2958299f3262fec2455ca56ed4e10fe6
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-04-07 15:48:44 +0200

    Create movieCard components

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MovieCard = () => {
+  return (
+    <View>
+      <Text>MovieCard</Text>
+    </View>
+  )
+}
+
+export default MovieCard
