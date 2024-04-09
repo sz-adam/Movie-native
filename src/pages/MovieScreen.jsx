019c323fa68646cdb8b3f762cf3adc4286e406da
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-04-09 18:56:38 +0200

    Revert "movie,series imdbDt  data request"
    This reverts commit 660e0f77cd7c3bbfcf7310c78d79b72aa9a82811.

--- src/pages/MovieScreen.jsx
+++ src/pages/MovieScreen.jsx
@@ -1,53 +1,12 @@
-import { Text, View, useWindowDimensions } from "react-native";
-import React, { useEffect, useState } from "react";
-import Header from "../components/Header";
-import Animated from "react-native-reanimated";
-import axios from "axios";
+import { View, Text } from 'react-native'
+import React from 'react'
 
-import { API_URL } from "@env";
-import { API_KEY } from "@env";
-
-export default function MovieScreen({ navigation, route }) {
+export default function MovieScreen({route}) {
   const { item } = route.params;
-  const { width } = useWindowDimensions();
-  const [dataDetails, setDataDetails] = useState("");
-
-  const imdbID = item?.imdbID;
-
-  useEffect(() => {
-    const fetchDetails = async () => {
-      try {
-        const details = await axios.get(`${API_URL}${API_KEY}&i=${imdbID}`);
-        console.log(details.data)
-        setDataDetails(details.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchDetails();
-  }, []);
-
+  console.log(item?.imdbID)
   return (
-    <View className="flex-1">
-      <Header />
-      <Animated.Image
-        sharedTransitionTag="sharedTag"
-        style={{ width: width, height: width }}
-        source={{ uri: dataDetails.Poster }}
-      />
-      <View>
-      {/* Itt jelenítsd meg a dataDetails-t */}
-      {/* Például, ha az adatok egy objektumot tartalmaznak, akkor így */}
-
-        <View className="absolute z-15 bg-black w-full h-screen border-t-2xl">
-          <Text className="color-white text-center text-2xl pt-2">{dataDetails.Title}</Text>
-       
-        </View>
-     
+    <View>
+      <Text>Movie</Text>
     </View>
-    </View>
-  );
+  )
 }
-
-
