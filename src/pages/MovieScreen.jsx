33178321b952efbfb7e1152718b60abb377b5a8a
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-04-09 19:08:19 +0200

    movie,series imdbDt data request

--- src/pages/MovieScreen.jsx
+++ src/pages/MovieScreen.jsx
@@ -1,12 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, View, useWindowDimensions } from "react-native";
+import React, { useEffect, useState } from "react";
+import Header from "../components/Header";
+import axios from "axios";
 
-export default function MovieScreen({route}) {
+import { API_URL } from "@env";
+import { API_KEY } from "@env";
+
+export default function MovieScreen({ route }) {
   const { item } = route.params;
-  console.log(item?.imdbID)
+  const { width } = useWindowDimensions();
+  const [dataDetails, setDataDetails] = useState("");
+
+  const imdbID = item?.imdbID;
+
+  useEffect(() => {
+    const fetchDetails = async () => {
+      try {
+        const details = await axios.get(`${API_URL}${API_KEY}&i=${imdbID}`);
+        setDataDetails(details.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchDetails();
+  }, []);
+
   return (
-    <View>
-      <Text>Movie</Text>
+    <View className="flex-1">
+      <Header />
+      <Image
+        sharedTransitionTag="sharedTag"
+        style={{ width: width, height: width }}
+        source={{ uri: dataDetails.Poster }}
+      />        
     </View>
-  )
+  );
 }
+
+
