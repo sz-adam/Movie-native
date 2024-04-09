d27f26d1e70f6f91b179fe582f53684d2819c25b
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-04-09 19:48:25 +0200

    style details

--- src/pages/MovieScreen.jsx
+++ src/pages/MovieScreen.jsx
@@ -1,10 +1,18 @@
-import { Text, View, useWindowDimensions } from "react-native";
+import {
+  Image,
+  Text,
+  View,
+  useWindowDimensions,
+  ScrollView,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import axios from "axios";
+import Icon from "react-native-vector-icons/AntDesign";
 
 import { API_URL } from "@env";
 import { API_KEY } from "@env";
+import { GlobalStyles } from "../constans/stlyes";
 
 export default function MovieScreen({ route }) {
   const { item } = route.params;
@@ -27,15 +35,66 @@ export default function MovieScreen({ route }) {
   }, []);
 
   return (
-    <View className="flex-1">
+    <View
+      className="flex-1"
+      style={{ backgroundColor: GlobalStyles.colors.gray500 }}
+    >
       <Header />
       <Image
         sharedTransitionTag="sharedTag"
         style={{ width: width, height: width }}
         source={{ uri: dataDetails.Poster }}
-      />        
+      />
+      <ScrollView>
+        <View>
+          <Text className="color-white text-center text-2xl py-2">
+            {dataDetails.Title}
+          </Text>
+          <View>
+            <View className="color-white py-2">
+              <Text className="color-white text-center">
+                Released : {dataDetails.Year} - {dataDetails.Runtime}
+              </Text>
+            </View>
+            <View className="color-white py-2">
+              <Text className="color-white text-center">
+                {dataDetails.Type} - {dataDetails.Genre}{" "}
+              </Text>
+            </View>
+            <View className="color-white  py-2">
+              <Text className="color-white text-center">
+              Language : {dataDetails.Language}{" "}
+              </Text>
+            </View>
+            <View className="color-white  py-2 ">
+              <Text className="color-white text-center">
+                {" "}
+                {dataDetails.Actors}{" "}
+              </Text>
+            </View>
+            <View className="color-white  py-2">
+              <Text className="color-white text-center">
+                <Icon name="star" color={GlobalStyles.colors.accent500} />{" "}
+                {dataDetails.imdbRating}{" "}
+              </Text>
+            </View>
+           {dataDetails.Type ==="series" ? (
+             <View className="color-white  py-2 ">
+             <Text className="color-white text-center">
+               {" "}
+               Season : {dataDetails.totalSeasons}{" "}
+             </Text>
+           </View>
+           ): ""}
+            <View className="color-white  py-2 m-5">
+              <Text className="color-white text-center">
+                {" "}
+                {dataDetails.Plot}{" "}
+              </Text>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
-
-
