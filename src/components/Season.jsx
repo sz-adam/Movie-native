3648b9bdf9b8499d3648c74a88893441cc2c8567
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-04-13 13:21:16 +0200

    showing the episodes of the season in a list

--- src/components/Season.jsx
+++ src/components/Season.jsx
@@ -1,13 +1,8 @@
 import React from "react";
-import { 
-  Text,
-  Image,
-  TouchableOpacity,
-} from "react-native";
+import { Text, Image, TouchableOpacity } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { GlobalStyles } from "../constans/styles";
 import { useNavigation } from "@react-navigation/native";
-
 
 const Season = ({ totalSeasons, poster }) => {
   const seasonsArray = Array.from(
@@ -18,12 +13,21 @@ const Season = ({ totalSeasons, poster }) => {
   const navigation = useNavigation();
 
   return (
-    <ScrollView horizontal showsHorizontalScrollIndicator={false} className="flex-row ">
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      className="flex-row "
+    >
       {seasonsArray.map((season) => (
         <TouchableOpacity
           key={season}
           className="mx-2 mb-2 text-center flex justify-center items-center "
-          onPress={() => navigation.navigate("SeasonSide", { season: season })}
+          onPress={() =>
+            navigation.navigate("SeasonSide", {
+              season: season,
+              poster: poster,
+            })
+          }
         >
           <Image source={{ uri: poster }} className="w-14 h-14 rounded-full" />
           <Text
--- src/pages/SeasonSide.jsx
+++ src/pages/SeasonSide.jsx
@@ -1,33 +1,67 @@
-import { View, Text } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { View, Text, FlatList, Image, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 import { API_URL } from "@env";
 import { API_KEY } from "@env";
+import { GlobalStyles } from "../constans/styles";
+import Icon from "react-native-vector-icons/Ionicons";
+import { useNavigation } from "@react-navigation/native";
 
-const SeasonSide = ({route}) => {
-    const { season } = route.params;
-    const [seasonSide, setSeasonSide] =useState([])
+const SeasonSide = ({ route }) => {
+  const navigation = useNavigation();
 
-    console.log(season)
+  const { season, poster } = route.params;
+  const [seasonSide, setSeasonSide] = useState([]);
 
-    useEffect(() => {
-        const fetchDetails = async () => {
-          try {
-            const details = await axios.get(`${API_URL}${API_KEY}&season=${season}&t=Arrow`);
-            console.log(details.data);
-          } catch (error) {
-            console.error("Error fetching data:", error);
-          }
-        };
-    
-        fetchDetails();
-      }, []);
-  return (
-    <View>
-      <Text>SeasonSide</Text>
+  useEffect(() => {
+    const fetchDetails = async () => {
+      try {
+        const details = await axios.get(
+          `${API_URL}${API_KEY}&season=${season}&t=Arrow`
+        );
+        setSeasonSide(details.data.Episodes);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchDetails();
+  }, []);
+
+  const renderEpisodeItem = ({ item }) => (
+    <View className="flex-1 m-2 rounded-lg shadow-md ">
+      <Image source={{ uri: poster }} className="w-full h-40 rounded-t-lg" />
+      <Text
+        className="text-lg font-semibold mt-2 text-center"
+        style={{ color: GlobalStyles.colors.primary50 }}
+      >
+        {item.Title}
+      </Text>
     </View>
-  )
-}
+  );
 
-export default SeasonSide
+  return (
+    <View
+      className="flex-1"
+      style={{ backgroundColor: GlobalStyles.colors.gray500 }}
+    >
+      <View className="m-2">
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          className="bg-white rounded-xl w-8 flex justify-center items-center"
+        >
+          <Icon name="arrow-back" size={30} />
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        data={seasonSide}
+        renderItem={renderEpisodeItem}
+        numColumns={2}
+        keyExtractor={(item) => item.imdbID || item.Title}
+      />
+    </View>
+  );
+};
+
+export default SeasonSide;
