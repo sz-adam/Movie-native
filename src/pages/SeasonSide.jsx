6a22d76de23ff1938c0fad17de352e8b65cca0eb
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-04-13 11:59:02 +0200

    retrieving the series of the season

--- src/pages/SeasonSide.jsx
+++ src/pages/SeasonSide.jsx
@@ -1,10 +1,28 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+
+import { API_URL } from "@env";
+import { API_KEY } from "@env";
 
 const SeasonSide = ({route}) => {
     const { season } = route.params;
+    const [seasonSide, setSeasonSide] =useState([])
 
     console.log(season)
+
+    useEffect(() => {
+        const fetchDetails = async () => {
+          try {
+            const details = await axios.get(`${API_URL}${API_KEY}&season=${season}&t=Arrow`);
+            console.log(details.data);
+          } catch (error) {
+            console.error("Error fetching data:", error);
+          }
+        };
+    
+        fetchDetails();
+      }, []);
   return (
     <View>
       <Text>SeasonSide</Text>
